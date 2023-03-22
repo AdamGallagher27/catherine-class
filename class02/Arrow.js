dc0d1d5e8fa77ad8dceaec78a1ab4dc4c2eca9e8
Adam1~ <n00211418@iadt.ie>
2023-03-22 13:31:16 +0000

    finished lab 1

---
+++ class02/Arrow.js
@@ -0,0 +1,42 @@
+//Arrow class
+class Arrow {
+	constructor(x, y, rotation) {
+		//x and y properties of arrow
+		this.x = x;
+		this.y = y;
+		//rotation of arrow
+		this.rotation = rotation;
+	}
+	//function to draw the arrow
+	drawArrow() {
+		push();
+
+		//translate from the top left corner to x and y of object
+		translate(this.x, this.y);
+		//rotate by rotation value of object
+		rotate(this.rotation);
+		// line(-50, -25, 0, -25);
+		// line(0, -25, 0, -50);
+		// line(0, -50, 50, 0);
+		// line(50, 0, 0, 50);
+		// line(0, 50, 0, 25);
+		// line(0, 25, -50, 25);
+		// line(-50, 25, -50, -25);
+
+		noStroke()
+		beginShape()
+		vertex(-50, -25)
+		vertex(0, -25)
+		vertex(0, -50)
+		vertex(50, 0)
+		vertex(0, 50)
+		vertex(0, 25)
+		vertex(-50, 25)
+		vertex(-50, -25)
+		endShape()
+		pop();
+		
+	}
+	
+
+}
